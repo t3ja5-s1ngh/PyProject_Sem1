c22dcb4a4207f455e71459f8c8a0f4926926b2b8
t3ja5_s1ngh <t3ja5.s1ngh@gmail.com>
2025-11-23 11:21:59 +0530

    pruning2

--- GUI.py
+++ GUI.py
@@ -1,80 +1,75 @@
 import tkinter as tk
-
+####
+####
+####
 class myGUI:
-
-    ###################################################################################
+################################################################################################################
     def __init__(self,control):
+    ########setup main window
         self.control=control
         self.root = tk.Tk()
         self.root.configure(bg="black")
-        self.root.title("Othello Board")
+        self.root.title("Othello")
         self.root.geometry("1000x1000+400+200")
-
-        self.canvas = tk.Canvas(self.root, width=896, height=896,
-                                bg="darkgreen", highlightthickness=3)
+    ########set canvas for board display
+        self.canvas = tk.Canvas(self.root, width=896, height=896,  bg="darkgreen", highlightthickness=3)
         self.canvas.pack(anchor="center")
-
-        self.btn_start = tk.Button(self.root, text="Start",command=lambda: self.start(self.btn_start, self.btn_load, self.status, self.btn_save))
+    ########set start button 
+        self.btn_start = tk.Button(self.root, text="Start",command=self.start)
         self.canvas.create_window(448, 400, window=self.btn_start)
-
-        self.btn_load = tk.Button(self.root, text="Load",command=lambda : self.load_game(self.control))
+    ########set load button
+        self.btn_load = tk.Button(self.root, text="Load",command=self.load_game)
         self.canvas.create_window(448, 448, window=self.btn_load)
-
-        self.status = tk.Label(self.root, text="Othello",
-                               font=("Arial", 12), bg="lightgray")
+    ########set status bar
+        self.status = tk.Label(self.root, text="Othello", font=("Arial", 12), bg="lightgray")
         self.status.place(x=40, y=900, width=920, height=50)
-
-        self.btn_save = tk.Button(self.root, text="Save",command=lambda : self.save_game(self.control))
-
-    ###################################################################################
-
+    ########set save button
+        self.btn_save = tk.Button(self.root, text="Save",command=control.save)
+################################################################################################################
     def draw_grid(self, w, h):
+    ########clear canvas and draw grid lines
         self.canvas.delete("all")
         for i in range(9):
             self.canvas.create_line(0, i*(w//8), w, i*(w//8), fill="black")
             self.canvas.create_line(i*(h//8), 0, i*(h//8), h, fill="black")
-
+################################################################################################################
     def play(self, row, col, color, blackscore, whitescore,turn):
+    ########draw a piece and update status bar  
         x1 = col * 112 + 10
         y1 = row * 112 + 10
         x2 = (col+1) * 112 - 10
         y2 = (row+1) * 112 - 10
         color="black" if color==1 else "white"
         turn="Black"  if turn==1 else "White"
-
         self.canvas.create_oval(x1, y1, x2, y2, fill=color)
         self.status.config(text=f"Black:{blackscore}              {turn}'s turn              White:{whitescore}")
-
-    def start(self, widget1, widget2, widget3, widget4):
-        widget1.destroy()
-        widget2.destroy()
-        widget3.config(text="Black:2                      White:2")
-        widget4.place(x=472, y=960)
+################################################################################################################
+    def start(self):
+    ########start a new game and bind click event
+        self.btn_save.place(x=472, y=960)
         self.canvas.bind("<Button-1>",self.click)
         self.draw_grid(896, 896)
         self.play(3, 3, 0, 2, 2,1)
         self.play(3, 4, 1, 2, 2,1)
         self.play(4, 3, 1, 2, 2,1)
         self.play(4, 4, 0, 2, 2,1)
-
+################################################################################################################
     def click(self, event):
+    ########calculate row and column from mouse click and pass to controller
         row = event.y // 112
         col = event.x // 112
-        control= self.control
-        control.handle_move(row, col)
-        # print("Clicked at:", row, col)
-
-    def load_game(self,control):
-        self.btn_start.destroy()
-        self.btn_load.destroy()
-        self.status.config(text="Black:2                      White:2")
+        self.control.handle_move(row, col)
+################################################################################################################
+    def load_game(self):
+    ########load a saved game and bind click event
         self.btn_save.place(x=472, y=960)
         self.canvas.bind("<Button-1>",self.click)
         self.draw_grid(896, 896)
-        control.load()
-
-    def save_game(self,control):
-        control.save()
-
+        self.control.load()
+################################################################################################################
     def run(self):
+    ########start the main event loop
        self.root.mainloop()
+###
+###
+###
--- main.py
+++ main.py
@@ -7,11 +7,13 @@ from GUI import myGUI
 #############################################################################################################################
 class Game:
     def __init__(self):
+    ########create objects to esatblish structure
         self.board = board.Board()
         self.gui =myGUI(self)
         self.gui.run()
 #############################################################################################################################
     def handle_move(self,row, col):
+    ########process a move and update GUI
         self.board.grid,self.board.player=logic.next_state(self.board.grid, row, col, self.board.player)
         self.board.count()
         for r in range(8):
@@ -20,12 +22,13 @@ class Game:
                     self.gui.play(r, c,self.board.grid[r][c], self.board.b_score, self.board.w_score,self.board.player)
 #############################################################################################################################
     def save(self):
+    ########save the current game state
         file_manager.save_game(self.board.grid, self.board.player)
 #############################################################################################################################
     def load(self):
-        board_state, current_player = file_manager.load_game()
-        self.board.grid = board_state
-        self.board.player = current_player
+     ########load a saved game and update GUI   
+        self.board.grid,self.board.player = file_manager.load_game()
+        self.board.count()
         for r in range(8):
             for c in range(8):
                 if self.board.grid[r][c] != 0:
@@ -33,4 +36,4 @@ class Game:
 #############################################################################################################################
 #############################################################################################################################
 #############################################################################################################################
-controller = Game()
+Game()
